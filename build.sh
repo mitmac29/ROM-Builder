937cdadfdfb964e6fce8c5b7cb798957acc6b57a
mitmac29 <118977337+mitmac29@users.noreply.github.com>
2023-04-13 01:30:54 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,21 +11,21 @@ else
 fi
 
 # Run the Extra Command
-$EXTRA_CMD
+
 
 # Prepare the Build Environment
-source build/envsetup.sh
+. build/envsetup.sh
 
 # lunch the target
 lunch ${LUNCH_COMBO} || { echo "ERROR: Failed to lunch the target!" && exit 1; }
 
 # Build the Code
 if [ -z "$J_VAL" ]; then
-    mka -j$(nproc --all) $TARGET || { echo "ERROR: Build Failed!" && exit 1; }
+    make -j$(nproc --all) $TARGET || { echo "ERROR: Build Failed!" && exit 1; }
 elif [ "$J_VAL"="0" ]; then
-    mka $TARGET || { echo "ERROR: Build Failed!" && exit 1; }
+    make $TARGET || { echo "ERROR: Build Failed!" && exit 1; }
 else
-    mka -j${J_VAL} $TARGET || { echo "ERROR: Build Failed!" && exit 1; }
+    make -j${J_VAL} $TARGET || { echo "ERROR: Build Failed!" && exit 1; }
 fi
 
 # Exit
