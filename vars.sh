a6f14d9e31652973d02e8d5cf564bf1fdf7ccb4c
mitmac29 <118977337+mitmac29@users.noreply.github.com>
2023-04-13 01:38:32 +0530

    Update vars.sh

--- vars.sh
+++ vars.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 
 # Manifest
-export MANIFEST=""
-export MANIFEST_BRANCH=""
+export MANIFEST="https://github.com/mitmac29/android.git"
+export MANIFEST_BRANCH="crd-11.0"
 
 # Device Info
-export DEVICE="" # Codename
-export OEM=""
-export PLATFORM="" # eg: sm6250, mt6765 etc.
+export DEVICE="lavender" # Codename
+export OEM="xiaomi"
+export PLATFORM="sdm660" # eg: sm6250, mt6765 etc.
 
 # Trees
-export DT_LINK=""
+export DT_LINK="https://github.com/mitmac29/device_xiaomi_lavender1.git -b arc"
 export DCT_LINK="" # Device Common Tree, Leave Blank if None
-export VT_LINK=""
+export VT_LINK="https://github.com/mitmac29/vendor_xiaomi_lavender1.git -b 11"
 export VCT_LINK="" # Vendor Common Tree, Leave Blank if None
-export KT_LINK="" # Kernel Tree, Leave Blank if Prebuilt
+export KT_LINK="https://github.com/mitmac29/kernel_xiaomi_lavender.git -b qti-oldcam" # Kernel Tree, Leave Blank if Prebuilt
 
 # Trees Path
 export DT_PATH="device/$OEM/$DEVICE"
 export DCT_PATH="device/$OEM/${PLATFORM}-common"
 export VT_PATH="vendor/$OEM/$DEVICE"
 export VCT_PATH="vendor/$OEM/${PLATFORM}-common"
-export KT_PATH="kernel/$OEM/$PLATFORM"
+export KT_PATH="kernel/$OEM/$DEVICE"
 
 # Lunch Combos and Targets
-export USE_BRUNCH_AS_TARGET=false # set it to "true" if you want to use brunch
+export USE_BRUNCH_AS_TARGET=true # set it to "true" if you want to use brunch
 export LUNCH_COMBO="" # eg: "aosp_miatoll-userdebug", No need if USE_BRUNCH_AS_TARGET is set to true
 export TARGET="" # eg: "bacon", No need if USE_BRUNCH_AS_TARGET is set to true
 
 # Output File (to upload)
-export OUTPUT_FILE="" # eg: "PixelExperience*.zip"
+export OUTPUT_FILE="Arc*.zip" # eg: "PixelExperience*.zip"
 
 # Extra Command
 export EXTRA_CMD=""
